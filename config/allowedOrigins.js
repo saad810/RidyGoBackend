b0d96a90cd2b56654468c7a26a6c27d0f493e450
Saad Amin <93635982+saad810@users.noreply.github.com>
2024-03-12 02:31:35 +0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'exp://192.168.1.3:8081',
-    'https://long-teal-lobster-kilt.cyclic.app'
+    'https://long-teal-lobster-kilt.cyclic.app',
+    '*'
 ]
 
 module.exports = allowedOrigins

0673716bc90141f375b48d187c02ded220b12db3
Saad Amin <93635982+saad810@users.noreply.github.com>
2024-03-15 17:37:44 +0500

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ mongoose
     });
 
     // Start the server
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5000;
     server.listen(port, () => {
       console.log(`Server is running on port http://localhost:${port}`);
     });

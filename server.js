f07c99863f73b3b4394951be2275f875f100ccf3
Saad Amin <93635982+saad810@users.noreply.github.com>
2024-03-15 17:30:11 +0500

    sockets update

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const http = require('http');
-const socketIo = require('socket.io');
 
 const corsOptions = require("./config/corsOptions");
 const OrderRoutes = require("./routes/OrderRoutes");
@@ -12,8 +10,6 @@ const UserRoutes = require("./routes/UserRoutes");
 const RiderRoutes = require("./routes/RiderRoutes");
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -29,29 +25,27 @@ app.use("/user", UserRoutes);
 app.use("/rider", RiderRoutes);
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Database connected");
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
 
-  // Socket.io logic
-  io.on('connection', (socket) => {
-    console.log('A client connected');
+    // Socket.io logic
+    io.on("connection", (socket) => {
+      console.log("A client connected");
 
-    // Handle disconnections
-    socket.on('disconnect', () => {
-        console.log('A client disconnected');
+      // Handle disconnections
     });
-  });
 
-  // Start the server
-  const port = process.env.PORT || 3000;
-  server.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+    // Start the server
+    const port = process.env.PORT || 3000;
+    server.listen(port, () => {
+      console.log(`Server is running on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
   });
-})
-.catch((err) => {
-  console.error("Database connection error:", err);
-});

124bcd4fc9a03f47edf310364a73c971467181c2
Saad Amin <93635982+saad810@users.noreply.github.com>
2024-03-16 19:34:58 +0500

    bugfix

--- server.js
+++ server.js
@@ -29,17 +29,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Database connected");
-
-    // Socket.io logic
-    io.on("connection", (socket) => {
-      console.log("A client connected");
-
-      // Handle disconnections
-    });
-
     // Start the server
     const port = process.env.PORT || 5000;
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log(`Server is running on port http://localhost:${port}`);
     });
   })
